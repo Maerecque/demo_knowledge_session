d9a2941dda5c392d7ab6db90fd385a6016e90768
Marc Zoomers <marczoomers@gmail.com>
2021-08-11 15:01:41 +0200

    New math functions

--- src/math_functions.py
+++ src/math_functions.py
@@ -1,3 +1,5 @@
+import cmath
+
 def complex_math(x: int, y: int) -> int:
     """Very complex math calculation. This function will add two numbers and return the result of the calculation.
 
@@ -12,3 +14,13 @@ def complex_math(x: int, y: int) -> int:
     answer = x + y
 
     return answer
+
+def less_complex_math(x: int, y: int) -> int:
+    answer = x - y
+    
+    return answer
+
+def calculate_complex_number(x: int, y: int) -> int:
+    complex_number = complex(x,y)
+    
+    return complex_number
