d1f1f0b915fa6fa56cd4bc5167a64fc8dd2286c7
Marc Zoomers <marczoomers@gmail.com>
2021-08-11 15:05:39 +0200

    Added documentation and new functions to main

--- src/math_functions.py
+++ src/math_functions.py
@@ -16,11 +16,29 @@ def complex_math(x: int, y: int) -> int:
     return answer
 
 def less_complex_math(x: int, y: int) -> int:
+    """A very (but not as the last) complex math calculation. This function will retract two numbers and return the result of the calculation.
+
+    Args:
+        x (int): first number to be calculated.
+        y (int): second number to be calculated.
+
+    Returns:
+        int: the answer to the calculation.
+    """    
     answer = x - y
     
     return answer
 
 def calculate_complex_number(x: int, y: int) -> int:
+    """This function will convert two number into a complex number.
+
+    Args:
+        x (int): first number to be calculated.
+        y (int): second number to be calculated.
+
+    Returns:
+        int: complex number
+    """    
     complex_number = complex(x,y)
     
     return complex_number
